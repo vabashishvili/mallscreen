3e63b763bb0498263bba8e503d56c257118d1f30
vabashishvili <vaxo.abashishvili@gmail.com>
2026-02-01 19:23:16 +0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,46 +1,24 @@
-const CACHE_NAME = 'molscreen-seamless-v1';
-
-// ფაილები, რომლებიც აუცილებლად უნდა დაქეშდეს
-const urlsToCache = [
-  './index.html'
-];
+const CACHE_NAME = 'molscreen-v5';
 
 self.addEventListener('install', (event) => {
   self.skipWaiting();
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
-  );
 });
 
 self.addEventListener('activate', (event) => {
-  // ძველი ქეშების წაშლა, რომ წარწერები ან ძველი შეცდომები არ დარჩეს
-  event.waitUntil(
-    caches.keys().then((cacheNames) => {
-      return Promise.all(
-        cacheNames.map((cache) => {
-          if (cache !== CACHE_NAME) {
-            return caches.delete(cache);
-          }
-        })
-      );
-    }).then(() => self.clients.claim())
-  );
+  event.waitUntil(clients.claim());
 });
 
 self.addEventListener('fetch', (event) => {
-  // ვიდეოების და საიტის ქეშირების ლოგიკა
   event.respondWith(
-    caches.match(event.request).then((response) => {
-      if (response) {
-        return response; // თუ ქეშშია, მომენტალურად ვაძლევთ
-      }
-      return fetch(event.request).then((fetchRes) => {
+    caches.match(event.request).then((res) => {
+      if (res) return res; // თუ ქეშშია, ეგრევე მიეცი (მომენტალურია)
+      
+      return fetch(event.request).then((networkRes) => {
         return caches.open(CACHE_NAME).then((cache) => {
-          // ვიწერთ Dropbox-ის ვიდეოებს მომავალი ოფლაინ მუშაობისთვის
-          if (event.request.url.includes('dropbox') || event.request.url.includes('index.html')) {
-            cache.put(event.request, fetchRes.clone());
+          if (event.request.url.includes('dropbox')) {
+            cache.put(event.request, networkRes.clone());
           }
-          return fetchRes;
+          return networkRes;
         });
       });
     })
